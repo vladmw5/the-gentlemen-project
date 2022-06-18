4211fe8d87ebc55c522c410f5868d7e46bc83e9f
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-06-18 12:23:00 +0300

    fixed-mobile-menu

--- src/js/components/mobile-menu.js
+++ src/js/components/mobile-menu.js
@@ -2,11 +2,13 @@ const btnOpen = document.querySelector('[data-open]');
 const mobileModal = document.querySelector('[data-modal]');
 const closeBtn = document.querySelector('[data-close]');
 const bodyEl = document.querySelector('body');
+const backdrop = document.querySelector('.mobile-menu-window');
 
 closeBtn.addEventListener('click', toggleMobileMenu);
 btnOpen.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu() {
   mobileModal.classList.toggle('is-open');
+  backdrop.classList.toggle('backdrop-mobile');
   bodyEl.classList.toggle('body-mobile-window');
 }
