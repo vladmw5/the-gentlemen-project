480ad0a15122df69d138d885759f29b6ff3ffeba
Oleksandr Palamarchuk <palamarchuk.oa93@gmail.com>
2022-06-18 12:23:53 +0300

    created navigation-modal

--- src/js/components/authentication-modal.js
+++ src/js/components/authentication-modal.js
@@ -1,14 +1,43 @@
-//Закриття модалки авторизації-реєстрації
+const backdrop = document.querySelector('.backdrop');
+const closeModalBtn = document.querySelector('.modal-button');
+const signUpBtn = document.querySelector('.sign-up-js');
+const logInBtn = document.querySelector('.log-in-js');
+const registrationForm = document.querySelector('.registration-js');
+const authorizationForm = document.querySelector('.authorization-js');
+const logOutItem = document.querySelector('.nav-list__item--exit');
+const libraryItem = document.querySelector('.nav-list__item--library');
 
-const refs = {
-    backdrop: document.querySelector('.backdrop'),
-    closeModalBtn: document.querySelector('.modal-button'),
+export { backdrop, signUpBtn, logInBtn, logOutItem, libraryItem};
+
+
+//Клік по кнопці SIGN UP і відкриття модального вікна з формою реєстрації
+
+signUpBtn.addEventListener('click', onSignUpClick);
+
+function onSignUpClick() {
+    removeBackdropHidden();
+    authorizationForm.classList.add('is-hidden-form');
+};
+
+//Клік по кнопці LOG IN відкриття модального вікна з формою авторизації
+
+logInBtn.addEventListener('click', onLogInClick);
+
+function onLogInClick() {
+    removeBackdropHidden();
+    registrationForm.classList.add('is-hidden-form');
 }
 
+//Закриття модального вікна
 
-
-refs.closeModalBtn.addEventListener('click', onCloseModalBtn);
+closeModalBtn.addEventListener('click', onCloseModalBtn);
 
 function onCloseModalBtn() {
-    refs.backdrop.classList.add('is-bck-hidden');
+    backdrop.classList.add('is-bck-hidden');
+    authorizationForm.classList.remove('is-hidden-form');
+    registrationForm.classList.remove('is-hidden-form');
+};
+
+function removeBackdropHidden() {
+     backdrop.classList.remove('is-bck-hidden');
 };
