2dbf1e07afcc524671b3178caa6867d726ebfb61
Vlad-Kozak <k911k12@gmail.com>
2022-06-19 14:45:48 +0300

    update

--- src/js/components/gallery.js
+++ src/js/components/gallery.js
@@ -1,5 +1,6 @@
 import { Spinner } from 'spin.js';
 
+import { optsForSpinner } from '../service/spinner-options';
 import { renderPaginationBar } from './pagination-bar';
 import { makeMarkupGallery, makeMarkupMovie } from '../service/gallery-markup';
 import {
@@ -8,38 +9,16 @@ import {
   getMoviesByID,
 } from '../service/gallery-requests';
 
-const optsForSpinner = {
-  lines: 8, // The number of lines to draw
-  length: 50, // The length of each line
-  width: 26, // The line thickness
-  radius: 63, // The radius of the inner circle
-  scale: 1.05, // Scales overall size of the spinner
-  corners: 0.5, // Corner roundness (0..1)
-  speed: 1.9, // Rounds per second
-  rotate: 0, // The rotation offset
-  animation: 'spinner-line-fade-default', // The CSS animation name for the lines
-  direction: 1, // 1: clockwise, -1: counterclockwise
-  color: '#000000', // CSS color or array of colors
-  fadeColor: 'transparent', // CSS color or array of colors
-  top: '50%', // Top position relative to parent
-  left: '50%', // Left position relative to parent
-  shadow: '0 0 1px transparent', // Box-shadow for the lines
-  zIndex: 2000000000, // The z-index (defaults to 2e9)
-  className: 'spinner', // The CSS class to assign to the spinner
-  position: 'absolute', // Element positioning
-};
-
 // vars
 export let firstTime = true;
 const ERR_CLASS = 'hidden-err';
 export const SESSION_STORAGE_USER_KEYWORD_KEY = 'user-search-keyword';
 
 // refs
-const form = document.querySelector('.hero-form');
 const gallery = document.querySelector('.gallery__list');
 const movieModal = document.querySelector('.filmcard-modal');
 const modalBackdrop = document.querySelector('.filmcard-modal-backdrop');
-const errorBlockRef = document.querySelector('.hero-form__text');
+const notifycation = document.querySelector('.hero-form__text');
 const inputSearchMovie = document.querySelector('.hero-form__input');
 
 // event Listener
@@ -69,13 +48,18 @@ export function renderMoviesByKeyword(keyword, page) {
   });
 }
 
+function renderMoviesByID(movieId) {
+  getMoviesByID(movieId).then(r => {
+    movieModal.innerHTML = makeMarkupMovie(r);
+  });
+}
+
 function onFormInput(e) {
   const keyword = e.target.value.trim();
-  errorBlockRef.classList.add(ERR_CLASS);
+  notifycation.classList.add(ERR_CLASS);
 
   if (!keyword) {
     firstRenderPopularMovies(1);
-    // errorBlockRef.classList.remove(ERR_CLASS);
     return;
   }
 
@@ -95,8 +79,14 @@ function onMovieClick(e) {
 
   const movieId = e.target.parentElement.dataset.id;
 
-  getMoviesByID(movieId).then(r => {
-    movieModal.innerHTML = makeMarkupMovie(r);
-    modalBackdrop.classList.remove('is-hidden');
-  });
+  renderMoviesByID(movieId);
+  toggleModal();
+
+  // const modalCloseBtn = document.querySelector('.filmcard-modal__close-btn');
+  // modalCloseBtn.addEventListener('click', toggleModal(), { once: true });
+}
+
+function toggleModal() {
+  document.body.classList.toggle('modal-open');
+  modalBackdrop.classList.toggle('is-hidden');
 }
