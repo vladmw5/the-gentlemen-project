f16248b12ffa37f548d7dc2f3caa985c84c653af
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-06-19 16:57:08 +0300

    add-header-burger-line

---
+++ src/js/components/burger-line.js
@@ -0,0 +1,24 @@
+const ulNavEl = document.querySelector('.modal-nav-list');
+const homeEl = document.querySelector('.link-home-js');
+const libraryEl = document.querySelector('.link-library-js');
+
+document.addEventListener('DOMContentLoaded', onCLickListNav);
+
+console.log(ulNavEl);
+
+function onCLickListNav() {
+  homeEl.classList.add('nav__link--before');
+  if (window.location.pathname === '/my-library.html') {
+    return addLineToListLibrary();
+  }
+}
+
+function addLineToListLibrary() {
+  const liEl = ulNavEl.children;
+  for (const i of liEl) {
+    if (i.classList.contains('modal-library-js')) {
+      libraryEl.classList.add('nav__link--before');
+      homeEl.classList.remove('nav__link--before');
+    }
+  }
+}
