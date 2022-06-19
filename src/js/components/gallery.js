2573172b392b527a14329c6d912d031b7f5ccb59
Vlad-Kozak <k911k12@gmail.com>
2022-06-19 15:35:51 +0300

    add closing modal by ESC and click on backdrop

--- src/js/components/gallery.js
+++ src/js/components/gallery.js
@@ -16,10 +16,11 @@ export const SESSION_STORAGE_USER_KEYWORD_KEY = 'user-search-keyword';
 
 // refs
 const gallery = document.querySelector('.gallery__list');
-const movieModal = document.querySelector('.filmcard-modal');
+const movieCase = document.querySelector('.filmcard__case');
 const modalBackdrop = document.querySelector('.filmcard-modal-backdrop');
 const notifycation = document.querySelector('.hero-form__text');
 const inputSearchMovie = document.querySelector('.hero-form__input');
+const modalCloseBtn = document.querySelector('.filmcard-modal__close-btn');
 
 // event Listener
 document.addEventListener('DOMContentLoaded', firstRenderPopularMovies(1));
@@ -33,25 +34,41 @@ const spinner = new Spinner(optsForSpinner).spin(gallery);
 // functions
 export function firstRenderPopularMovies(page) {
   getPopularMovies(page).then(r => {
-    makeMarkupGallery(r.results).then(r => {
-      gallery.innerHTML = r;
-    });
-    renderPaginationBar(r.total_pages, page);
+    makeMarkupGallery(r.results)
+      .then(r => {
+        gallery.innerHTML = r;
+        renderPaginationBar(r.total_pages, page);
+      })
+      .catch(console.log);
   });
 }
 
 export function renderMoviesByKeyword(keyword, page) {
   sessionStorage.setItem(SESSION_STORAGE_USER_KEYWORD_KEY, keyword);
   getMoviesByKeyword(keyword, page).then(r => {
-    makeMarkupGallery(r.results).then(r => (gallery.innerHTML = r));
-    renderPaginationBar(r.total_pages, page);
+    makeMarkupGallery(r.results)
+      .then(r => {
+        if (r.length === 0) {
+          document.querySelector('.pagination-bar').innerHTML = '';
+          gallery.innerHTML =
+            '<p class="notifycation__text">No results! Sorry =(</p>';
+          return;
+        }
+
+        gallery.innerHTML = r;
+        renderPaginationBar(r.total_pages, page);
+      })
+      .catch(console.log);
   });
 }
 
 function renderMoviesByID(movieId) {
-  getMoviesByID(movieId).then(r => {
-    movieModal.innerHTML = makeMarkupMovie(r);
-  });
+  getMoviesByID(movieId)
+    .then(r => {
+      movieCase.innerHTML = makeMarkupMovie(r);
+      toggleModal();
+    })
+    .catch(console.log);
 }
 
 function onFormInput(e) {
@@ -80,10 +97,22 @@ function onMovieClick(e) {
   const movieId = e.target.parentElement.dataset.id;
 
   renderMoviesByID(movieId);
-  toggleModal();
-
-  // const modalCloseBtn = document.querySelector('.filmcard-modal__close-btn');
   // modalCloseBtn.addEventListener('click', toggleModal(), { once: true });
+  window.addEventListener('keydown', closeMovieModalByEsc, { once: true });
+  modalBackdrop.addEventListener('click', closeMovieModalByClickBackdrop);
+}
+
+function closeMovieModalByEsc(e) {
+  if (e.code === 'Escape') {
+    toggleModal();
+  }
+}
+
+function closeMovieModalByClickBackdrop(e) {
+  if (e.target === e.currentTarget) {
+    toggleModal();
+    modalBackdrop.removeEventListener('click', closeMovieModalByClickBackdrop);
+  }
 }
 
 function toggleModal() {
