8f6d87bb9530576453c5173adbe676e005fd9542
Vladyslav Avidzba <wasssd1@gmail.com>
2022-06-20 19:15:30 +0300

    pagination-fix-2

--- src/js/components/pagination-bar.js
+++ src/js/components/pagination-bar.js
@@ -1,4 +1,4 @@
-import { markup } from '../service/pagination-bar-markup';
+// import { markup } from '../service/pagination-bar-markup';
 import {
   firstTime,
   firstRenderPopularMovies,
@@ -6,6 +6,46 @@ import {
   SESSION_STORAGE_USER_KEYWORD_KEY,
 } from './gallery';
 
+const markup = {
+  leftArrow() {
+    return /*html*/ `<div
+      class="pagination-bar__element pagination-bar__control pagination-bar__left"
+      data-type="left"
+    >
+      <svg class="arrow-icon arrow-left-icon">
+        <use href="./images/symbol-defs.svg#icon-arrow-left2"></use>
+      </svg>
+    </div>`;
+  },
+  number(n, isActive = false, hideOnMobile = false) {
+    return /*html*/ `<li
+      class="pagination-bar__element pagination-bar__number ${
+        isActive ? 'pagination-bar__element--active' : ''
+      } ${hideOnMobile ? 'pagination-bar__element--mobile-hidden' : ''}"
+      data-page="${n}"
+      data-type="number"
+    >
+      <span class='no-click'>${n}</span>
+    </li>`;
+  },
+  dots(hideOnMobile = false) {
+    return /*html*/ `<li class="pagination-bar__element pagination-bar__dots ${
+      hideOnMobile ? 'pagination-bar__element--mobile-hidden' : ''
+    }" data-type="dots"> 
+      <span>...</span>
+    </li>`;
+  },
+  rightArrow() {
+    return /*html*/ `<div
+      class="pagination-bar__element pagination-bar__control pagination-bar__right"
+      data-type="right"
+    >
+      <svg class="arrow-icon arrow-right-icon">
+        <use href="./images/symbol-defs.svg#icon-arrow-right2"></use>
+      </svg>
+    </div>`;
+  },
+};
 const BAR_HIDDEN_CLASS = 'pagination-bar__element--hidden';
 
 const paginationBarRef = document.querySelector('.pagination-bar__list');
