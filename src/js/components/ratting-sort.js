28d8040564ccdd2691abce303a0b41657f8b81fd
Vladyslav Avidzba <75910284+vladmw5@users.noreply.github.com>
2022-06-21 12:44:39 +0300

    Revert "Fix sort and filter"

--- src/js/components/ratting-sort.js
+++ src/js/components/ratting-sort.js
@@ -1,27 +1,17 @@
-import { onSortClick, onResetSortAndFilterClick } from './gallery';
 import { resetBtn } from './filter-menu';
 const ratingBtn = document.querySelector('.rating');
 
+console.log(ratingBtn);
+
 document.addEventListener('homePageLoaded', () => {
   ratingBtn.addEventListener('click', addClassOpen);
-
   resetBtn.addEventListener('click', () => {
     resetBtn.setAttribute('disabled', 'disabled');
     ratingBtn.classList.remove('rating-active');
-    onResetSortAndFilterClick();
   });
 
-  function addClassOpen(e) {
+  function addClassOpen() {
     resetBtn.removeAttribute('disabled');
-
     ratingBtn.classList.toggle('rating-active');
-
-    if (ratingBtn.dataset.rating === 'false') {
-      ratingBtn.dataset.rating = 'true';
-    } else {
-      ratingBtn.dataset.rating = 'false';
-    }
-
-    onSortClick(e);
   }
 });
